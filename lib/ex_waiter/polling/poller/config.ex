c4f8de0dafe1c9395a5494286d59a962e2058ca3
David Baldwin <baldwindavid@gmail.com>
2023-02-23 11:24:37 -0500

    Add typespecs for 0 arity config functions and bump version

--- lib/ex_waiter/polling/poller/config.ex
+++ lib/ex_waiter/polling/poller/config.ex
@@ -1,8 +1,10 @@
 defmodule ExWaiter.Polling.Poller.Config do
   @type polling_result :: :ok | :error | {:ok, any()} | {:error, any()} | boolean()
-  @type polling_fn :: (__MODULE__.t() -> polling_result())
-  @type delay :: (__MODULE__.t() -> non_neg_integer()) | non_neg_integer()
-  @type max_attempts :: non_neg_integer() | :infinity | (__MODULE__.t() -> boolean())
+  @type polling_fn :: (__MODULE__.t() -> polling_result()) | (() -> polling_result())
+  @type delay ::
+          (__MODULE__.t() -> non_neg_integer()) | (() -> non_neg_integer()) | non_neg_integer()
+  @type max_attempts ::
+          non_neg_integer() | :infinity | (__MODULE__.t() -> boolean()) | (() -> boolean())
 
   defstruct [
     :polling_fn,
